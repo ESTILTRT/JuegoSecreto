abd69e20ede1c2dd6c275aaf2611c8fc41ccf116
ESTILTRT <157448750+ESTILTRT@users.noreply.github.com>
2024-02-18 22:37:19 -0600

    Arreglo app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ function verificarIntento(){
 
     console.log(intentos);
     if(numeroDeUsuario === numeroSecreto){
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${intentos == 1 ? "vez":"veces" }`);
+        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${intentos == 1 ? "intento":"intentos" }`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     }else{
         if (numeroDeUsuario > numeroSecreto) {
